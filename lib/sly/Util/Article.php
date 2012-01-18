d89aec2f1325b894d0fc4141c4c6f8b4dec0cb24
Christoph <github@xrstf.de>
2012-01-19 00:29:09 +0100

    let addOns extend our frontend router rule set

---
+++ lib/sly/Util/Article.php
@@ -0,0 +1,115 @@
+<?php
+/*
+ * Copyright (c) 2012, webvariants GbR, http://www.webvariants.de
+ *
+ * This file is released under the terms of the MIT license. You can find the
+ * complete text in the attached LICENSE file or online at:
+ *
+ * http://www.opensource.org/licenses/mit-license.php
+ */
+
+/**
+ * @ingroup util
+ *
+ * @author zozi@webvariants.de
+ */
+class sly_Util_Article {
+	/**
+	 * checks wheter an article exists or not
+	 *
+	 * @param  int $articleId
+	 * @return boolean
+	 */
+	public static function exists($articleId) {
+		return self::isValid(self::findById($articleId));
+	}
+
+	/**
+	 * @param  mixed $article
+	 * @return boolean
+	 */
+	public static function isValid($article) {
+		return is_object($article) && ($article instanceof sly_Model_Article);
+	}
+
+	/**
+	 * @param  int $articleId
+	 * @param  int $clang
+	 * @return sly_Model_Article
+	 */
+	public static function findById($articleId, $clang = null) {
+		return sly_Service_Factory::getArticleService()->findById($articleId, $clang);
+	}
+
+	/**
+	 * @param  int $clang
+	 * @return sly_Model_Article
+	 */
+	public static function findSiteStartArticle($clang = null) {
+		return self::findById(sly_core::getSiteStartArticleId(), $clang);
+	}
+
+	/**
+	 * @param  int     $categoryId
+	 * @param  boolean $ignore_offlines
+	 * @param  int     $clangId
+	 * @return array
+	 */
+	public static function findByCategory($categoryId, $ignore_offlines = false, $clangId = null) {
+		return sly_Service_Factory::getArticleService()->findArticlesByCategory($categoryId, $ignore_offlines, $clangId);
+	}
+
+	/**
+	 * @param  string  $type
+	 * @param  boolean $ignore_offlines
+	 * @param  int     $clangId
+	 * @return array
+	 */
+	public static function findByType($type, $ignore_offlines = false, $clangId = null) {
+		return sly_Service_Factory::getArticleService()->findArticlesByType($type, $ignore_offlines, $clangId);
+	}
+
+	/**
+	 * @param  boolean $ignore_offlines
+	 * @param  int     $clang
+	 * @return array
+	 */
+	public static function getRootArticles($ignore_offlines = false, $clang = false) {
+		return self::findByCategory(0, $ignore_offlines, $clang);
+	}
+
+	/**
+	 * @param  sly_Model_Article $article
+	 * @return boolean
+	 */
+	public static function isSiteStartArticle(sly_Model_Article $article) {
+		return $article->getId() == sly_Core::getSiteStartArticleId();
+	}
+
+	/**
+	 * @param  sly_Model_Article $article
+	 * @return boolean
+	 */
+	public static function isNotFoundArticle(sly_Model_Article $article) {
+		return $article->getId() == sly_Core::getNotFoundArticleId();
+	}
+
+	public static function canReadArticle(sly_Model_User $user, $articleId) {
+		return sly_Util_Category::canReadCategory($user, $articleId);
+	}
+
+	public static function canEditArticle(sly_Model_User $user, $articleId) {
+		if ($user->isAdmin()) return true;
+		return sly_Authorisation::hasPermission($user->getId(), 'article', 'edit', $articleId);
+	}
+
+	public static function canEditContent(sly_Model_User $user, $articleId) {
+		if ($user->isAdmin()) return true;
+		return $user->hasRight('article', 'editcontent', $articleId);
+	}
+
+	public static function getUrl($articleId, $clang = null, $params = array()) {
+		$article = self::findById($articleId, $clang);
+		return $article ? $article->getUrl($params) : '';
+	}
+}
