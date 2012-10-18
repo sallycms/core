745631a00ecf0a661ed972e964ffa8f61b44fe6a
Christoph <github@xrstf.de>
2012-10-18 02:01:10 +0200

    added sly_Request, based on Symfony2's HttpFoundation\Request class

---
+++ lib/sly/App/Base.php
@@ -0,0 +1,264 @@
+<?php
+/*
+ * Copyright (c) 2012, webvariants GbR, http://www.webvariants.de
+ *
+ * This file is released under the terms of the MIT license. You can find the
+ * complete text in the attached LICENSE file or online at:
+ *
+ * http://www.opensource.org/licenses/mit-license.php
+ */
+
+abstract class sly_App_Base implements sly_App_Interface {
+	/**
+	 * initialize the app
+	 */
+	public function initialize() {
+		$isSetup = sly_Core::isSetup();
+
+		// boot addOns
+		if (!$isSetup) sly_Core::loadAddOns();
+
+		// register listeners
+		sly_Core::registerListeners();
+
+		// synchronize develop
+		if (!$isSetup) $this->syncDevelopFiles();
+	}
+
+	/**
+	 * call an action on a controller
+	 *
+	 * @param  mixed  $controller  a controller name (string) or a prebuilt controller instance
+	 * @param  string $action
+	 * @return sly_Response
+	 */
+	public function dispatch($controller, $action) {
+		$pageResponse = $this->tryController($controller, $action);
+
+		// register the new response, if the controller returned one
+		if ($pageResponse instanceof sly_Response) {
+			sly_Core::setResponse($pageResponse);
+		}
+
+		// if the controller returned another action, execute it
+		if ($pageResponse instanceof sly_Response_Action) {
+			$pageResponse = $pageResponse->execute($this);
+		}
+
+		return $pageResponse;
+	}
+
+	/**
+	 * call an action on a controller
+	 *
+	 * @param  mixed  $controller  a controller name (string) or a prebuilt controller instance
+	 * @param  string $action
+	 * @return sly_Response
+	 */
+	public function tryController($controller, $action) {
+		// build controller instance and check permissions
+		try {
+			if (!($controller instanceof sly_Controller_Interface)) {
+				$className  = $this->getControllerClass($controller);
+				$controller = $this->getController($className);
+			}
+
+			if (!$controller->checkPermission($action)) {
+				throw new sly_Authorisation_Exception(t('page_not_allowed', $action, get_class($controller)), 403);
+			}
+		}
+		catch (Exception $e) {
+			return $this->handleControllerError($e, $controller, $action);
+		}
+
+		// generic controllers should have no safety net and *must not* throw exceptions.
+		if ($controller instanceof sly_Controller_Generic) {
+			return $this->runController($controller, 'generic', $action);
+		}
+
+		// classic controllers should have a basic exception handling provided by us.
+		try {
+			return $this->runController($controller, $action);
+		}
+		catch (Exception $e) {
+			return $this->handleControllerError($e, $controller, $action);
+		}
+	}
+
+	/**
+	 * call an action on a controller
+	 *
+	 * @param  mixed  $controller  a controller name (string) or a prebuilt controller instance
+	 * @param  string $action
+	 * @param  mixed  $param       a single parameter for the action method
+	 * @return sly_Response
+	 */
+	protected function runController($controller, $action, $param = null) {
+		ob_start();
+
+		// prepare controller
+		$method = $action.'Action';
+
+		// run the action method
+		if ($param === null) {
+			$r = $controller->$method();
+		}
+		else {
+			$r = $controller->$method($param);
+		}
+
+		if ($r instanceof sly_Response || $r instanceof sly_Response_Action) {
+			ob_end_clean();
+			return $r;
+		}
+
+		// collect output
+		return ob_get_clean();
+	}
+
+	protected function syncDevelopFiles() {
+		$user = sly_Core::isBackend() ? sly_Util_User::getCurrentUser() : null;
+
+		if (sly_Core::isDeveloperMode() || ($user && $user->isAdmin())) {
+			sly_Service_Factory::getTemplateService()->refresh();
+			sly_Service_Factory::getModuleService()->refresh();
+			sly_Service_Factory::getAssetService()->validateCache();
+		}
+	}
+
+	/**
+	 * check if a controller exists
+	 *
+	 * @param  string $controller
+	 * @return boolean
+	 */
+	public function isControllerAvailable($controller) {
+		return class_exists($this->getControllerClass($controller));
+	}
+
+	/**
+	 * return classname for &page=whatever
+	 *
+	 * It will return sly_Controller_System for &page=system
+	 * and sly_Controller_System_Languages for &page=system_languages
+	 *
+	 * @param  string $controller
+	 * @return string
+	 */
+	public function getControllerClass($controller) {
+		$className = $this->getControllerClassPrefix();
+		$parts     = explode('_', $controller);
+
+		foreach ($parts as $part) {
+			$className .= '_'.ucfirst($part);
+		}
+
+		return $className;
+	}
+
+	/**
+	 * fire an event about the current controller
+	 *
+	 * This fires the SLY_CONTROLLER_FOUND event.
+	 *
+	 * @param boolean $useCompatibility  if true, PAGE_CHECKED will be fired as well
+	 */
+	protected function notifySystemOfController($useCompatibility = false) {
+		$name       = $this->getCurrentControllerName();
+		$controller = $this->getCurrentController();
+		$params     = array(
+			'app'    => $this,
+			'name'   => $name,
+			'action' => $this->getCurrentAction()
+		);
+
+		sly_Core::dispatcher()->notify('SLY_CONTROLLER_FOUND', $controller, $params);
+
+		if ($useCompatibility) {
+			// backwards compatibility for pre-0.6 code
+			sly_Core::dispatcher()->notify('PAGE_CHECKED', $name);
+		}
+	}
+
+	/**
+	 * handle a controller that printed its output
+	 *
+	 * @param sly_Response $response
+	 * @param string       $content
+	 * @param string       $appName
+	 */
+	protected function handleStringResponse(sly_Response $response, $content, $appName) {
+		// collect additional output (warnings and notices from the bootstrapping)
+		while (ob_get_level()) $content = ob_get_clean().$content;
+
+		$config     = sly_Core::config();
+		$dispatcher = sly_Core::dispatcher();
+		$content    = $dispatcher->filter('OUTPUT_FILTER', $content, array('environment' => $appName));
+		$etag       = substr(md5($content), 0, 12);
+		$useEtag    = $config->get('USE_ETAG');
+
+		if ($useEtag === true || $useEtag === $appName) {
+			$response->setEtag($etag);
+		}
+
+		$response->setContent($content);
+		$response->isNotModified();
+	}
+
+	/**
+	 * get controller by name
+	 *
+	 * @throws sly_Controller_Exception
+	 * @param  string $className
+	 * @return sly_Controller_Interface  the controller
+	 */
+	protected function getController($className) {
+		static $instances = array();
+
+		if (!isset($instances[$className])) {
+			if (!class_exists($className)) {
+				throw new sly_Controller_Exception(t('unknown_controller', $className), 404);
+			}
+
+			if (class_exists('ReflectionClass')) {
+				$reflector = new ReflectionClass($className);
+
+				if ($reflector->isAbstract()) {
+					throw new sly_Controller_Exception(t('unknown_controller', $className), 404);
+				}
+			}
+
+			$instance = new $className();
+
+			if (!($instance instanceof sly_Controller_Interface)) {
+				throw new sly_Controller_Exception(t('does_not_implement', $className, 'sly_Controller_Interface'), 404);
+			}
+
+			$instances[$className] = $instance;
+		}
+
+		return $instances[$className];
+	}
+
+	/**
+	 * get the current controller
+	 *
+	 * @return sly_Controller_Interface
+	 */
+	public function getCurrentController() {
+		$name = $this->getCurrentControllerName();
+
+		if (mb_strlen($name) === 0) {
+			return null;
+		}
+
+		$className  = $this->getControllerClass($name);
+		$controller = $this->getController($className);
+
+		return $controller;
+	}
+
+	abstract public function getControllerClassPrefix();
+
+	abstract protected function handleControllerError(Exception $e, $controller, $action);
+}
---
+++ lib/sly/Form/Widget/MediaList.php
@@ -0,0 +1,108 @@
+<?php
+/*
+ * Copyright (c) 2012, webvariants GbR, http://www.webvariants.de
+ *
+ * This file is released under the terms of the MIT license. You can find the
+ * complete text in the attached LICENSE file or online at:
+ *
+ * http://www.opensource.org/licenses/mit-license.php
+ */
+
+/**
+ * Medialist widget
+ *
+ * This element will render a special widget that allows the user to select
+ * a list of files from the mediapool.
+ *
+ * @ingroup form
+ * @author  Christoph
+ */
+class sly_Form_Widget_MediaList extends sly_Form_Widget_MediaBase implements sly_Form_IElement {
+	protected $min = 0;
+	protected $max = -1;
+
+	/**
+	 * Set minimum number of required files
+	 *
+	 * @param int $min
+	 */
+	public function setMinElements($min) {
+		$min = (int) $min;
+		$this->min = $min <= 0 ? 0 : $min;
+
+		if ($this->min > $this->max) {
+			$this->max = $this->min;
+		}
+	}
+
+	/**
+	 * Set maximum number of allowed files
+	 *
+	 * @param int $max  (-1 means 'no limit')
+	 */
+	public function setMaxElements($max) {
+		$max = (int) $max;
+		$this->max = $max < 0 ? -1 : $max;
+
+		if ($this->min > $this->max) {
+			$this->min = $this->max;
+		}
+	}
+
+	/**
+	 * Get minimum number of required files
+	 *
+	 * @return int
+	 */
+	public function getMinElements() {
+		return $this->min;
+	}
+
+	/**
+	 * Get maximum number of allowed files
+	 *
+	 * @return int
+	 */
+	public function getMaxElements() {
+		return $this->max;
+	}
+
+	/**
+	 * Constructor
+	 *
+	 * @param string $name   the element name
+	 * @param string $label  the label
+	 * @param array  $value  the current value (a list of filenames)
+	 * @param string $id     optional HTML ID
+	 */
+	public function __construct($name, $label, $value, $id = null) {
+		parent::__construct($name, $label, $value, $id);
+		$this->addOuterClass('sly-form-medialistwidget-row');
+	}
+
+	/**
+	 * Render the element
+	 *
+	 * @return string  the rendered XHTML
+	 */
+	public function render() {
+		$this->attributes['value'] = $this->getDisplayValue();
+		return $this->renderFilename('element/widget/medialist.phtml');
+	}
+
+	/**
+	 * Returns the value to be displayed
+	 *
+	 * This method will return the values that shall be displayed in the form.
+	 * This is mostly useful when a form is submitted and the POST data will be
+	 * shown instead of those that were given when the form elements are
+	 * initialized.
+	 *
+	 * @return array  a list of filenames
+	 */
+	public function getDisplayValue() {
+		$files = $this->getDisplayValueHelper('string', false);
+		if ($files === null) return array();
+		return is_array($files) ? $files : explode(',', $files);
+	}
+}
