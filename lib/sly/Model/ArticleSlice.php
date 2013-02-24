70f80d2f6f424309948aee7a6f296d26f079bf54
Christoph <github@xrstf.de>
2013-02-24 02:23:31 +0100

    merge with 0.8

---
+++ lib/sly/Model/ArticleSlice.php
@@ -0,0 +1,283 @@
+<?php
+/*
+ * Copyright (c) 2013, webvariants GbR, http://www.webvariants.de
+ *
+ * This file is released under the terms of the MIT license. You can find the
+ * complete text in the attached LICENSE file or online at:
+ *
+ * http://www.opensource.org/licenses/mit-license.php
+ */
+
+/**
+ * Business Model for ArticleSlices
+ *
+ * @author  zozi@webvariants.de
+ * @ingroup model
+ */
+class sly_Model_ArticleSlice extends sly_Model_Base_Id implements sly_Model_ISlice {
+	protected $article_id;
+	protected $clang;
+	protected $slot;
+	protected $slice_id;
+	protected $pos;
+	protected $createdate;
+	protected $updatedate;
+	protected $createuser;
+	protected $updateuser;
+	protected $revision;
+	protected $slice;   ///< sly_Model_Slice
+	protected $article; ///< sly_Model_Article
+
+	protected $_attributes = array(
+		'updateuser' => 'string',
+		'createuser' => 'string',
+		'createdate' => 'datetime',
+		'updatedate' => 'datetime',
+		'pos'        => 'int',
+		'article_id' => 'int',
+		'clang'      => 'int',
+		'slot'       => 'string',
+		'slice_id'   => 'int',
+		'revision'   => 'int'
+	); ///< array
+
+	/**
+	 *
+	 * @return int
+	 */
+	public function getArticleId() {
+		return $this->article_id;
+	}
+
+	/**
+	 *
+	 * @return int
+	 */
+	public function getClang() {
+		return $this->clang;
+	}
+
+	/**
+	 *
+	 * @return string
+	 */
+	public function getSlot() {
+		return $this->slot;
+	}
+
+	/**
+	 *
+	 * @return int
+	 */
+	public function getPosition() {
+		return $this->pos;
+	}
+
+	/**
+	 *
+	 * @return int
+	 */
+	public function getSliceId() {
+		return $this->slice_id;
+	}
+
+	/**
+	 *
+	 * @return int
+	 */
+	public function getCreateDate() {
+		return $this->createdate;
+	}
+
+	/**
+	 *
+	 * @return int
+	 */
+	public function getUpdateDate() {
+		return $this->updatedate;
+	}
+
+	/**
+	 *
+	 * @return string
+	 */
+	public function getCreateUser() {
+		return $this->createuser;
+	}
+
+	/**
+	 *
+	 * @return int
+	 */
+	public function getUpdateUser() {
+		return $this->updateuser;
+	}
+
+	/**
+	 *
+	 * @return int
+	 */
+	public function getRevision() {
+		return $this->revision;
+	}
+
+	/**
+	 *
+	 * @return sly_Model_Article
+	 */
+	public function getArticle() {
+		if (empty($this->article)) {
+			$this->article =  sly_Util_Article::findById($this->getArticleId(), $this->getClang());
+		}
+		return $this->article;
+	}
+
+	/**
+	 *
+	 * @return sly_Model_Slice
+	 */
+	public function getSlice() {
+		if (empty($this->slice)) {
+			$this->slice = sly_Core::getContainer()->getSliceService()->findById($this->getSliceId());
+		}
+		return $this->slice;
+	}
+
+	public function getPrevious() {
+		$container = sly_Core::getContainer();
+		$service   = $container->getArticleSliceService();
+		$db        = $container->getPersistence();
+
+		return $service->findOne(sprintf('slot = %s AND pos < %d AND article_id = %d AND clang = %d ORDER BY pos DESC', $db->quote($this->getSlot()), $this->getPosition(), $this->getArticleId(), $this->getClang()));
+	}
+
+	public function getNext() {
+		$container = sly_Core::getContainer();
+		$service   = $container->getArticleSliceService();
+		$db        = $container->getPersistence();
+
+		return $service->findOne(sprintf('slot = %s AND pos > %d AND article_id = %d AND clang = %d ORDER BY pos ASC', $db->quote($this->getSlot()), $this->getPosition(), $this->getArticleId(), $this->getClang()));
+	}
+
+	public function getModule() {
+		return $this->getSlice()->getModule();
+	}
+
+	public function setModule($module) {
+		$slice = $this->getSlice();
+		$slice->setModule($module);
+		$this->slice = sly_Core::getContainer()->getSliceService()->save($slice);
+	}
+
+	/**
+	 *
+	 * @param sly_Model_Slice $slice
+	 */
+	public function setSlice(sly_Model_Slice $slice) {
+		$this->slice    = $slice;
+		$this->slice_id = $slice->getId();
+	}
+
+	/**
+	 *
+	 * @param type $slot
+	 */
+	public function setSlot($slot) {
+		$this->slot = $slot;
+	}
+
+	/**
+	 *
+	 * @param sly_Model_Article $article
+	 */
+	public function setArticle(sly_Model_Article $article) {
+		$this->article = $article;
+		$this->article_id = $article->getId();
+		$this->clang = $article->getClang();
+	}
+
+	/**
+	 * @param string $updateuser
+	 */
+	public function setUpdateUser($updateuser) {
+		$this->updateuser = $updateuser;
+	}
+
+	/**
+	 * @param mixed $updatedate  unix timestamp or date using 'YYYY-MM-DD HH:MM:SS' format
+	 */
+	public function setUpdateDate($updatedate) {
+		$this->updatedate = sly_Util_String::isInteger($updatedate) ? (int) $updatedate : strtotime($updatedate);
+	}
+
+	/**
+	 * @param string $createuser
+	 */
+	public function setCreateUser($createuser) {
+		$this->createuser = $createuser;
+	}
+
+	/**
+	 * @param mixed $createdate  unix timestamp or date using 'YYYY-MM-DD HH:MM:SS' format
+	 */
+	public function setCreateDate($createdate) {
+		$this->createdate = sly_Util_String::isInteger($createdate) ? (int) $createdate : strtotime($createdate);
+	}
+
+	/**
+	 *
+	 * @param int $position
+	 */
+	public function setPosition($position) {
+		$this->pos = (int) $position;
+	}
+
+	/**
+	 * @param  string $finder
+	 * @param  string $value
+	 */
+	public function setValue($finder, $value = null) {
+		$this->getSlice()->setValue($finder, $value);
+	}
+
+	public function setValues($values = array()) {
+		return $this->getSlice()->setValues($values);
+	}
+
+	/**
+	 * @param  string $type
+	 * @param  string $finder
+	 * @return mixed
+	 */
+	public function getValue($finder, $default = null) {
+		return $this->getSlice()->getValue($finder, $default);
+	}
+
+	public function getValues() {
+		return $this->getSlice()->getValues();
+	}
+
+	/**
+	 * get the rendered output
+	 *
+	 * @return string
+	 */
+	public function getOutput() {
+		$values   = $this->getValues();
+		$renderer = new sly_Slice_Renderer($this->getModule(), $values);
+		$output   = $renderer->renderOutput($this);
+		return $output;
+	}
+
+	public function setRevision($revision) {
+		$this->revision = (int) $revision;
+	}
+
+	/**
+	 * drop slice from serialized instance
+	 */
+	public function __sleep() {
+		$this->slice   = null;
+		$this->article = null;
+	}
+}
---
+++ lib/sly/Service/Language.php
@@ -0,0 +1,220 @@
+<?php
+/*
+ * Copyright (c) 2013, webvariants GbR, http://www.webvariants.de
+ *
+ * This file is released under the terms of the MIT license. You can find the
+ * complete text in the attached LICENSE file or online at:
+ *
+ * http://www.opensource.org/licenses/mit-license.php
+ */
+
+/**
+ * @author  christoph@webvariants.de
+ * @ingroup service
+ */
+class sly_Service_Language extends sly_Service_Model_Base_Id {
+	protected $tablename = 'clang'; ///< string
+	protected $cache;               ///< BabelCache_Interface
+	protected $dispatcher;          ///< sly_Event_IDispatcher
+
+	/**
+	 * Constructor
+	 *
+	 * @param sly_DB_Persistence    $persistence
+	 * @param BabelCache_Interface  $cache
+	 * @param sly_Event_IDispatcher $dispatcher
+	 */
+	public function __construct(sly_DB_Persistence $persistence, BabelCache_Interface $cache, sly_Event_IDispatcher $dispatcher) {
+		parent::__construct($persistence);
+
+		$this->cache      = $cache;
+		$this->dispatcher = $dispatcher;
+	}
+
+	/**
+	 * @param  array $params
+	 * @return sly_Model_Language
+	 */
+	protected function makeInstance(array $params) {
+		return new sly_Model_Language($params);
+	}
+
+	/**
+	 * @param  int $articleId
+	 * @param  int $clang
+	 * @return sly_Model_Language
+	 */
+	public function findById($languageID) {
+		$languages  = $this->findAll();
+		$languageID = (int) $languageID;
+
+		return isset($languages[$languageID]) ? $languages[$languageID] : null;
+	}
+
+	/**
+	 * @param  boolean $keysOnly
+	 * @return array
+	 */
+	public function findAll($keysOnly = false) {
+		$languages = $this->cache->get('sly.language', 'all', null);
+
+		if ($languages === null) {
+			$list      = $this->find(null, null, 'id');
+			$languages = array();
+
+			foreach ($list as $language) {
+				$languages[$language->getId()] = $language;
+			}
+
+			$this->cache->set('sly.language', 'all', $languages);
+		}
+
+		return $keysOnly ? array_keys($languages) : $languages;
+	}
+
+	/**
+	 * @param  sly_Model_Base $model
+	 * @return sly_Model_Base
+	 */
+	public function save(sly_Model_Base $model) {
+		$this->cache->delete('sly.language', 'all');
+
+		$result = parent::save($model);
+
+		// notify listeners
+		$this->dispatcher->notify('CLANG_UPDATED', $model);
+
+		return $result;
+	}
+
+	/**
+	 * @throws Exception           if something goes wrong
+	 * @param  array $params
+	 * @return sly_Model_Language
+	 */
+	public function create($params) {
+		$langs = sly_Util_Language::findAll(); // TODO: avoid wrapper around ourselves
+
+		// if there are no languages yet, don't attempt to copy anything
+
+		if (count($langs) === 0) {
+			$newLanguage = parent::create($params);
+		}
+		else {
+			$sql    = $this->getPersistence();
+			$ownTrx = !$sql->isTransRunning();
+
+			if ($ownTrx) {
+				$sql->beginTransaction();
+			}
+
+			try {
+				$newLanguage = parent::create($params);
+				$sourceID    = sly_Core::getDefaultClangId();
+
+				// if a bad default language was configured, use the first one
+				if (!isset($langs[$sourceID])) {
+					$ids = array_keys($langs);
+					sort($ids);
+					$sourceID = reset($ids);
+				}
+
+				$sql->query(str_replace('~', sly_Core::getTablePrefix(),
+					'INSERT INTO ~article (id,re_id,name,catname,catpos,attributes,'.
+					'startpage,pos,path,status,createdate,updatedate,type,clang,createuser,'.
+					'updateuser,revision) '.
+					'SELECT id,re_id,name,catname,catpos,attributes,startpage,pos,path,0,createdate,'.
+					'updatedate,type,?,createuser,updateuser,revision '.
+					'FROM ~article WHERE clang = ?'),
+					array($newLanguage->getId(), $sourceID)
+				);
+
+				if ($ownTrx) {
+					$sql->commit();
+				}
+			}
+			catch (Exception $e) {
+				if ($ownTrx) {
+					$sql->rollBack();
+				}
+
+				throw $e;
+			}
+		}
+
+		// update cache before notifying the listeners (so that they can call findAll() and get fresh data)
+		$langs[$newLanguage->getId()] = $newLanguage;
+		$this->cache->set('sly.language', 'all', $langs);
+
+		// notify listeners
+		$this->dispatcher->notify('CLANG_ADDED', $newLanguage, array('id' => $newLanguage->getId(), 'language' => $newLanguage));
+
+		return $newLanguage;
+	}
+
+	/**
+	 * @throws sly_Exception
+	 * @param  sly_Model_Language  $language
+	 * @return int
+	 */
+	public function deleteByLanguage(sly_Model_Language $language) {
+		return $this->deleteById($language->getId());
+	}
+
+	/**
+	 * @param  array $where
+	 * @return int
+	 */
+	public function delete($where) {
+		$db = $this->getPersistence();
+
+		// find all languages first
+		$toDelete = $this->find($where);
+		$allLangs = sly_Util_Language::findAll(); // TODO: avoid wrapper around ourselves
+
+		// delete
+		$res = parent::delete($where);
+
+		// update cache (so that addOns can access fresh clang data when listening to CLANG_DELETED)
+		foreach ($toDelete as $language) {
+			unset($allLangs[$language->getId()]);
+		}
+
+		$this->cache->set('sly.language', 'all', $allLangs);
+
+		// remove
+		$db     = $this->getPersistence();
+		$ownTrx = !$db->isTransRunning();
+
+		if ($ownTrx) {
+			$db->beginTransaction();
+		}
+
+		try {
+			foreach ($toDelete as $language) {
+				$params = array('clang' => $language->getId());
+				$db->delete('article', $params);
+				$db->delete('article_slice', $params);
+
+				$this->dispatcher->notify('CLANG_DELETED', $language, array(
+					'id'   => $language->getId(),
+					'name' => $language->getName()
+				));
+			}
+
+			if ($ownTrx) {
+				$db->commit();
+			}
+		}
+		catch (Exception $e) {
+			if ($ownTrx) {
+				$db->rollBack();
+			}
+
+			throw $e;
+		}
+
+		sly_Core::clearCache();
+		return $res;
+	}
+}
