d7dc933964c047a4aecfcfa632b17ab4ac8bf861
Christoph <github@xrstf.de>
2013-08-30 03:28:44 +0200

    this is 0.10 from now on

---
+++ lib/sly/I18N/Base.php
@@ -0,0 +1,34 @@
+<?php
+/*
+ * Copyright (c) 2013, webvariants GbR, http://www.webvariants.de
+ *
+ * This file is released under the terms of the MIT license. You can find the
+ * complete text in the attached LICENSE file or online at:
+ *
+ * http://www.opensource.org/licenses/mit-license.php
+ */
+
+/**
+ * I18N interface
+ *
+ * @ingroup i18n
+ * @author  Christoph
+ * @since   0.3
+ */
+interface sly_I18N_Base {
+	/**
+	 * Translate a key
+	 *
+	 * @param  string $key
+	 * @return string
+	 */
+	public function msg($key);
+
+	/**
+	 * Check if a message exists
+	 *
+	 * @param  string $key
+	 * @return boolean
+	 */
+	public function hasMsg($key);
+}
