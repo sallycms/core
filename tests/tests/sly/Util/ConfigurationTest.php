7daa1d61b92b9a26fc766b5e98b112ce2488c425
Zozi <zozi@webvariants.de>
2016-01-28 22:55:36 +0100

    fix unit tests and add some more tests

---
+++ tests/tests/sly/Util/ConfigurationTest.php
@@ -0,0 +1,24 @@
+<?php
+/*
+ * Copyright (c) 2014, webvariants GmbH & Co. KG, http://www.webvariants.de
+ *
+ * This file is released under the terms of the MIT license. You can find the
+ * complete text in the attached LICENSE file or online at:
+ *
+ * http://www.opensource.org/licenses/mit-license.php
+ */
+
+class sly_Util_ConfigurationTest extends PHPUnit_Framework_TestCase {
+	
+	public function testLoadYamlFile() {
+		$config = new sly_Configuration();
+		$config->setContainer(sly_Core::getContainer());
+		
+		sly_Util_Configuration::loadYamlFile($config, __DIR__.'/../../../config/sly_local.yml', true);
+		sly_Util_Configuration::loadYamlFile($config, __DIR__.'/../../../config/sly_project.yml', false);
+		
+		$this->assertArrayHasKey('dbname', $config->get('database'));
+		$this->assertArrayHasKey('templates', $config->get('/'));
+	}
+			
+}
