430d8d290b0ca7af8cb1efac873951c3d73afda2
Zozi <zozi@webvariants.de>
2016-09-13 12:49:40 +0200

    replace pdo persistence with doctrine dbal persistencex

--- lib/sly/Container.php
+++ lib/sly/Container.php
@@ -117,7 +117,7 @@ class sly_Container extends Pimple implements Countable {
 			return new sly_DB_PDO_Connection($driver, $pdo);
 		});
 
-		$this['sly-persistence'] = $this->inject(function($container) {
+		$this['sly-persistence-pdo'] = $this->inject(function($container) {
 			$config     = $container['sly-config']->get('database');
 			$connection = $container['sly-pdo-connection'];
 
@@ -125,6 +125,15 @@ class sly_Container extends Pimple implements Countable {
 			// a fresh instance for every access. We should refactor the database access
 			// to allow for a single persistence instance.
 			return new sly_DB_PDO_Persistence($config['driver'], $connection, $config['table_prefix']);
+		});
+
+		$this['sly-persistence'] = $this->inject(function($container) {
+			$connection = $container['sly-dbal-connection'];
+
+			// TODO: to support the iterator inside the persistence, we need to create
+			// a fresh instance for every access. We should refactor the database access
+			// to allow for a single persistence instance.
+			return new sly\Database\Persistence($connection);
 		});
 
 		$this['sly-cache-factory'] = $this->share(function($container) {
@@ -343,6 +352,15 @@ class sly_Container extends Pimple implements Countable {
 			return new sly_I18N(null, null, false);
 		});
 
+		$this['sly-dbal-connection'] = $this->share(function($container) {
+			$config = $container['sly-config']->get('database');
+
+			$config['driver']       = 'pdo_'.$config['driver'];
+			$config['wrapperClass'] = 'sly\Database\Connection';
+
+			return Doctrine\DBAL\DriverManager::getConnection($config);
+		});
+
 		//////////////////////////////////////////////////////////////////////////
 		// allow to overwrite default recipes
 
@@ -544,7 +562,7 @@ class sly_Container extends Pimple implements Countable {
 	}
 
 	/**
-	 * @return sly_DB_PDO_Persistence
+	 * @return sly_DB_Persistence
 	 */
 	public function getPersistence() {
 		return $this['sly-persistence'];
@@ -794,6 +812,14 @@ class sly_Container extends Pimple implements Countable {
 	 */
 	public function getFilehasher() {
 		return $this->get('sly-filehasher');
+	}
+
+	/**
+	 *
+	 * @return sly\Database\Connection
+	 */
+	public function getDBALConnection() {
+		return $this->get('sly-dbal-connection');
 	}
 
 	/*          setters for objects that are commonly set          */
---
+++ lib/sly/Database/Connection.php
@@ -0,0 +1,83 @@
+<?php
+/*
+ * Copyright (c) 2016, webvariants GmbH & Co. KG, http://www.webvariants.de
+ *
+ * This file is released under the terms of the MIT license. You can find the
+ * complete text in the attached LICENSE file or online at:
+ *
+ * http://www.opensource.org/licenses/mit-license.php
+ */
+
+namespace sly\Database;
+
+use Doctrine\DBAL\Connection as DoctrineConnection;
+use Doctrine\DBAL\Driver;
+use Doctrine\DBAL\Configuration;
+use Doctrine\Common\EventManager;
+
+class Connection extends DoctrineConnection {
+	protected $prefix;
+
+	public function __construct(array $params, Driver $driver, Configuration $config = null, EventManager $eventManager = null) {
+		$this->prefix = $params['table_prefix'];
+
+		parent::__construct($params, $driver, $config, $eventManager);
+	}
+
+	public function getDatabasePlatformVersion() {
+		// Driver does not support version specific platforms.
+		if (!$this->_driver instanceof VersionAwarePlatformDriver) {
+			return null;
+		}
+
+		// Explicit platform version requested (supersedes auto-detection).
+		if (isset($this->_params['serverVersion'])) {
+			return $this->_params['serverVersion'];
+		}
+
+		// If not connected, we need to connect now to determine the platform version.
+		if (null === $this->_conn) {
+			$this->connect();
+		}
+
+		// Automatic platform version detection.
+		if ($this->_conn instanceof ServerInfoAwareConnection &&
+				!$this->_conn->requiresQueryForServerVersion()
+		) {
+			return $this->_conn->getServerVersion();
+		}
+
+		// Unable to detect platform version.
+		return null;
+	}
+
+	public function getPrefix() {
+		return $this->prefix;
+	}
+
+	public function getTable($tableExpression) {
+		return $this->getPrefix() . $tableExpression;
+	}
+
+	public function delete($tableExpression, array $identifier, array $types = array()) {
+		$tableExpression = $this->getTable($tableExpression);
+
+		parent::delete($tableExpression, $identifier, $types);
+	}
+
+	public function update($tableName, array $data, array $identifier, array $types = array()) {
+		$tableExpression = $this->getTable($tableExpression);
+
+		parent::update($tableName, $data, $identifier, $types);
+	}
+
+	public function insert($tableExpression, array $data, array $types = array()) {
+		$tableExpression = $this->getTable($tableExpression);
+
+		parent::insert($tableExpression, $data, $types);
+	}
+
+	public function createQueryBuilder() {
+		return parent::createQueryBuilder();
+	}
+}
